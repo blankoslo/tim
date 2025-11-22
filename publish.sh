faa9b05befc5eca4633fe32e7e92dbcc8a660602
John Korsnes <john.korsnes@blank.no>
2025-11-22 17:45:15 +0100

    bugfix: release.yml, draft-releases do not contain artifacts

--- publish.sh
+++ publish.sh
@@ -46,5 +46,4 @@ gh release create $VERSION \
   './out/tim-win.tar.gz#tim-win' \
   "./out/BlankDev.Tools.Tim.${VERSION}.nupkg#tim-tool-nuget" \
   --title "$VERSION" \
-  --generate-notes \
-  --draft
+  --generate-notes 
